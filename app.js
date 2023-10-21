ecbe3b4d030af320047dccbd5b0f2429c87c8b6a
Mark <markandrews2023@gmail.com>
2023-10-20 21:01:22 -0400

    building the website

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-let btn = document.getElementById('btnClick')
-let image = document.getElementById('image')
-btn.addEventListener('click', function() {
+let btn = document.getElementById('genBtn')
+btn.addEventListener('click', fetchAndMakeMeme);
+
+function createMemeDiv(memeUrl) {
+    const memeDiv = document.createElement('div');
+    memeDiv.classList.add('memeDiv');
+
+    const memeImg = createMemeImg(memeUrl);
+    const deleteBtn = createDeleteBtn(memeDiv);
+
+    memeDiv.appendChild(memeImg);
+    memeDiv.appendChild(deleteBtn);
+    return memeDiv;
+}
+
+function createMemeImg(memeUrl) {
+    const memeImg = document.createElement('img');
+    memeImg.setAttribute('src', memeUrl);
+    memeImg.classList.add('memeStyle')
+    return memeImg;
+}
+
+function createDeleteBtn(memeDiv) {
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = "Delete Meme";
+    deleteButton.classList.add('deleteBtn');
+    deleteButton.addEventListener('click', function () {
+        memeDiv.remove();
+    });
+    return deleteButton
+}
+
+function fetchAndMakeMeme() {
     fetch("https://meme-api.com/gimme/wholesomememes")
-       .then(res => res.json())
-       .then(result => {
-        console.log(result)
-        image.src = result.message;
-       })
-       .catch(err=>console.log(err))
-})
+        .then((response) => response.json())
+        .then((data) => {
+            let memeImgUrl = data.url;
+            const memeDiv = createMemeDiv(memeImgUrl);
+            let memeContainer = document.getElementById('memeContainer');
+            memeContainer.appendChild(memeDiv)
+        })
+        // .catch(err => console.log(err))
+}
